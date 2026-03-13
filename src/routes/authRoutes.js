382d88884eb50f2b22dd5a77b6b2f1b6f9474a9f
Brtsz8 <bartoszpacyga06@gmail.com>
2026-03-13 16:51:22 +0100

    login endpoint

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -28,17 +28,32 @@ router.post('/register', (req,res) => {
 
         const token = jwt.sign({id: result.lastInsertRowid}, process.env.JWT_SECRET, {expiresIn: '24h'})
 
-        res.json({ token })
+        res.status(201).json({ token })
     } catch (err) {
-        console.log(err)
+        console.log(err.message)
         res.sendStatus(503) //server fault 
     }
-
-    res.sendStatus(201)
 })
 
 router.post('/login', (req,res) => { 
+    const { username, password } = req.body
 
+    try {
+        const getUser = db.prepare('SELECT * FROM users WHERE username = ?')
+        const user = getUser.get(username)
+
+        if(!user) {return res.status(404).send({ message : "USER NOT FOUND"})}
+
+        const passwordIsValid = bcrypt.compareSync(password, user.password)
+        if(!passwordIsValid) {return res.status(401).send({ message : "INVALID PASSWORD"})}
+
+        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: '24h'})
+        return res.json({ token })
+
+    } catch (err) {
+        console.log(err.message)
+        res.sendStatus(503) //server fault 
+    }
 })
 
 module.exports = router
